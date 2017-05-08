08c512bd5e2a657a500e34795ed30f17f4c25d9d
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-05-08 17:30:03 -0500

    Add search bar and weather list components

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
-const apiKey = '4b614cdd2d47d0b14d3eb1dd90c3a095';
+import axios from 'axios';
+
+const API_KEY = '4b614cdd2d47d0b14d3eb1dd90c3a095';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},pe`;
+  const request = axios.get(url);
+
+  console.log('request', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
